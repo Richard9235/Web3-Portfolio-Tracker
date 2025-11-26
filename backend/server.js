f0839a31e23ea216f58dd6517657aca2802f200a
Richard9235 <richardllmajarucon@gmail.com>
2025-11-26 11:15:47 +0800

    C: React initialization

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../Frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get("/",(req,res)=>{
     console.log("GET RESPOSNSE")
